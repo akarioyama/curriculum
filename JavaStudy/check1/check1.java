d4fcdaee5bd8251cbd2940544ad9495551b717e3
akarioyama <113684071+akarioyama@users.noreply.github.com>
2022-10-21 17:51:44 +0900

    課題提出テスト
    check1.java の提出

--- JavaStudy/check1/check1.java
+++ JavaStudy/check1/check1.java
@@ -3,31 +3,31 @@
  * チェックテスト-Java1章
  *
  */
-public class Check {
+public class Check1 {
     public static void main(String[] args) {
         /*
          * 問1
          * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
-         */int[] data = { 3, 1, 2, 7, 5 };
-
+         */
+        int[] data = { 3, 1, 2, 7, 5 };
         /*
          * 問2
          * 以下のfor文を完成させなさい
          */
-        for (int i = 0; i < /* ここに記述 */data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < /* ここに記述 */data.length; i++) {
-            for (int j = /* ここに記述 */data.length; j > i; j--) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = data.length; j > i; j--) {
                 /*
                  * 問3
                  * 以下、配列の添字を入れてソートを完成させなさい
                  */
-                if (data[0/* ここに記述 */] > data[1/* ここに記述 */]) {
-                    int box = data[2/* ここに記述 */];
-                    data[3/* ここに記述 */] = data[4/* ここに記述 */];
-                    data[5/* ここに記述 */] = box;
+                if (data[j - 1] > data[j]) {
+                    int box = data[j];
+                    data[j] = data[j - 1];
+                    data[j - 1] = box;
                 }
             }
         }
