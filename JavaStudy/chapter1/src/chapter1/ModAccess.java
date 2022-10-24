718835040708654769803927f2377aa1c57cdb71
akarioyama <113684071+akarioyama@users.noreply.github.com>
2022-10-24 16:03:51 +0900

    ,

---
+++ JavaStudy/chapter1/src/chapter1/ModAccess.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+public class ModAccess {
+	public String pub_var = "publicだよ";
+	protected String prot_var = "protectedだよ";
+    String pack_var = "package privateだよ";
+	private String pri_var = "privateだよ";
+
+    public void test() {
+    	System.out.println(this.pub_var);
+	    System.out.println(this.prot_var);
+	    System.out.println(this.pack_var);
+	    System.out.println(this.pri_var);
+	}
+
+	public static void main(String[] args) {
+		ModAccess m = new ModAccess();
+	    m.test();
+	}
+}
