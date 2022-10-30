c507604e4cce63a58887d42010460b096831f128
akarioyama <113684071+akarioyama@users.noreply.github.com>
2022-10-30 21:29:46 +0900

    課題提出テスト
    ２－１の課題

--- JavaStudy/2-1/study/src/study/Main.java
+++ JavaStudy/2-1/study/src/study/Main.java
@@ -2,28 +2,29 @@ package study;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String firstName = "akari";
-        String lastName = "oyama";
+		String firstName = "akari";
+		String lastName = "oyama";
 
-        System.out.println(getName(firstName, lastName));
-        System.out.println();
+		System.out.println(getName(firstName, lastName));
+		System.out.println();
 
+		int[] arr = { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+		for (int i = 11; i < 20; i++) {
+			if (i % 2 == 1) {
+				System.out.println(i + "は奇数です。");
+			}
+		}
+	}
 
-        int [] arr = { 11, 12, 13, 14, 15, 16, 17, 18, 18, 19, 20 };
-        int i = 0;
-        for ( i = 0; i<11; i++) {
-          }
-    }
+	public static String getName(String firstName, String lastName) {
+		return "firstName" + "lastName";
+	}
 
-    public static String getName(String firstName, String lastName) {
-        return firstName + lastName ;
-    }
-
-    public static void isOdd(int num) {
-        if (num % 2 != 0) {
-            System.out.println(num + "は奇数です。");
-        }
-    }
+	public static void isOdd(int num) {
+		if (num % 2 != 0) {
+			System.out.println(num + "は奇数です。");
+		}
+	}
 }
