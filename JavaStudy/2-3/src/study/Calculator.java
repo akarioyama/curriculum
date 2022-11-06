bcb6fdf7c6816612e26909daefed5980bc818f15
akarioyama <113684071+akarioyama@users.noreply.github.com>
2022-11-06 22:18:56 +0900

    課題提出テスト
    2-3の作成

---
+++ JavaStudy/2-3/src/study/Calculator.java
@@ -0,0 +1,18 @@
+package study;
+
+public class Calculator {
+
+	int plus= 10;
+
+    protected  int plus(int a ) {
+        return a+ 1;
+    }
+
+    protected int plus(int a, int b) {
+        return a+ b;
+    }
+
+    protected int plus(int a, int b, int c) {
+        return a + b + c;
+    }
+}
